b6315d850d57eda4808888c0a255603661720044
Edward Tierra <edrichtierra@gmail.com>
2016-02-20 14:02:23 -0800

    add controllers

---
+++ client/js/controller.js
@@ -0,0 +1,12 @@
+app.controller("HomeController", function($scope, $location){
+
+});
+app.controller("ContactController", function($scope, $location){
+
+});
+app.controller("AboutController", function($scope, $location){
+
+});
+app.controller("PostsController", function($scope, $location){
+
+});
