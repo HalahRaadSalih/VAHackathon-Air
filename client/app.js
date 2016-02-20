c85a3b8fa49eb6179d3efe6d9b92de61f18b2e99
Edward Tierra <edrichtierra@gmail.com>
2016-02-20 14:00:17 -0800

    add app.js

---
+++ client/app.js
@@ -0,0 +1,18 @@
+var app = angular.module('air', ['ngRoute']);
+app.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: "./views/templates/home.html",
+        controller: "HomeController"
+    }).when('/posts', {
+        templateUrl: "./views/templates/posts.html",
+        controller: "PostsController"
+    }).when('/contact', {
+        templateUrl: "./views/templates/contact.html",
+        controller: "ContactController"
+    }).when('/about', {
+        templateUrl: "./views/templates/about.html",
+        controller: "AboutController"
+    }).otherwise({
+        redirectTo: "/"
+    })
+});
